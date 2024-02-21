58614580209e5cc556d8ab58aecf524b33364b0c
jhethia <jhethia@gmail.com>
2024-02-21 15:14:36 +0530

    Invalid Ticket Cases Handle

--- src/main/java/org/LLD/Services/ParkingServiceIMPL.java
+++ src/main/java/org/LLD/Services/ParkingServiceIMPL.java
@@ -10,88 +10,73 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ParkingServiceIMPL implements ParkingService{
+public class ParkingServiceIMPL implements ParkingService {
 
     UtilAutowire utilAutowire = new UtilAutowire();
     RepositoryAutowire repositoryAutowire = new RepositoryAutowire();
+
     @Override
     public String createParkingLot(String parkingLotId, Integer numberOfFloors, Integer numberOfSpots) {
 
         int spotIndex = 1;
-        for (int floor = 1; floor <= numberOfFloors; floor++){
+        for (int floor = 1; floor <= numberOfFloors; floor++) {
 
-            Map<Integer,ParkingSpot> spotsInFloor = new HashMap<>();
-            for (int spot = 1; spot <= numberOfSpots; spot++){
+            Map<Integer, ParkingSpot> spotsInFloor = new HashMap<>();
+            for (int spot = 1; spot <= numberOfSpots; spot++) {
 
-                ParkingSpot parkingSpot = ParkingSpot.builder()
-                        .spotId(spot)
-                        .parkingFloorId(floor)
-                        .parkingLotId(parkingLotId)
-                        .spotState(SpotState.free)
-                        .spotType(VehicleType.CAR)
-                        .build();
+                ParkingSpot parkingSpot = ParkingSpot.builder().spotId(spot).parkingFloorId(floor).parkingLotId(parkingLotId).spotState(SpotState.free).spotType(VehicleType.CAR).build();
 
-                if(spot == 1){
+                if (spot == 1) {
                     parkingSpot.setSpotType(VehicleType.TRUCK);
-                } else if (spot ==2 || spot==3) {
+                } else if (spot == 2 || spot == 3) {
                     parkingSpot.setSpotType(VehicleType.BIKE);
                 }
 
-                repositoryAutowire.getParkingSpotRepository().getParkingSpotMap().get(parkingSpot.getSpotType()).put(spotIndex,parkingSpot);
+                repositoryAutowire.getParkingSpotRepository().getParkingSpotMap().get(parkingSpot.getSpotType()).put(spotIndex, parkingSpot);
                 spotIndex++;
-                spotsInFloor.put(spot,parkingSpot);
+                spotsInFloor.put(spot, parkingSpot);
             }
 
-            ParkingFloor parkingFloor = ParkingFloor.builder()
-                    .parkingFloorId(floor)
-                    .parkingLotId(parkingLotId)
-                    .parkingSpots(spotsInFloor)
-                    .build();
-            repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().put(floor,parkingFloor);
+            ParkingFloor parkingFloor = ParkingFloor.builder().parkingFloorId(floor).parkingLotId(parkingLotId).parkingSpots(spotsInFloor).build();
+            repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().put(floor, parkingFloor);
         }
 
 
-        ParkingLot parkingLot = ParkingLot.builder()
-                .parkingLotId(parkingLotId)
-                .parkingFloors(repositoryAutowire.getParkingFloorRepository().getParkingFloorMap())
-                .build();
+        ParkingLot parkingLot = ParkingLot.builder().parkingLotId(parkingLotId).parkingFloors(repositoryAutowire.getParkingFloorRepository().getParkingFloorMap()).build();
 
-        return "Created parking lot with "+repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().size()+" floors and "+repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().get(2).getParkingSpots().size()+" slots per floor";
+        return "Created parking lot with " + repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().size() + " floors and " + repositoryAutowire.getParkingFloorRepository().getParkingFloorMap().get(2).getParkingSpots().size() + " slots per floor";
     }
 
     @Override
     public void displayFreeCount(VehicleType vehicleType) {
-        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getFreeCountResult(vehicleType,repositoryAutowire.getParkingSpotRepository());
-        utilAutowire.getDisplayUtil().displayFreeCount(result,vehicleType);
+        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getFreeCountResult(vehicleType, repositoryAutowire.getParkingSpotRepository());
+        utilAutowire.getDisplayUtil().displayFreeCount(result, vehicleType);
     }
 
     @Override
     public void displayFreeSlots(VehicleType vehicleType) {
-        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getFreeSlotsResult(vehicleType,repositoryAutowire.getParkingSpotRepository());
-        utilAutowire.getDisplayUtil().displayFreeSlots(result,vehicleType);
+        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getFreeSlotsResult(vehicleType, repositoryAutowire.getParkingSpotRepository());
+        utilAutowire.getDisplayUtil().displayFreeSlots(result, vehicleType);
 
     }
 
     @Override
     public void displayOccupiedSlots(VehicleType vehicleType) {
-        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getOccupiedSlotsResult(vehicleType,repositoryAutowire.getParkingSpotRepository());
-        utilAutowire.getDisplayUtil().displayOccupiedSlots(result,vehicleType);
+        Map<Integer, List<Integer>> result = utilAutowire.getFindingUtil().getOccupiedSlotsResult(vehicleType, repositoryAutowire.getParkingSpotRepository());
+        utilAutowire.getDisplayUtil().displayOccupiedSlots(result, vehicleType);
 
     }
+
     @Override
     public String parkVehicle(VehicleType vehicleType, String vehicleRegNumber, String vehicleColor) {
 
-        VehicleEntity vehicle = VehicleEntity.builder()
-                .vehicleType(vehicleType)
-                .vehicleRegNumber(vehicleRegNumber)
-                .vehicleColor(vehicleColor)
-                .build();
+        VehicleEntity vehicle = VehicleEntity.builder().vehicleType(vehicleType).vehicleRegNumber(vehicleRegNumber).vehicleColor(vehicleColor).build();
 
-        ParkingSpot spot = utilAutowire.getFindingUtil().getSlotVehicle(vehicle.getVehicleType(),repositoryAutowire.getParkingSpotRepository());
+        ParkingSpot spot = utilAutowire.getFindingUtil().getSlotVehicle(vehicle.getVehicleType(), repositoryAutowire.getParkingSpotRepository());
 
         String res = "Could Not Park Vehicle.";
-        if(spot!=null){
-            ParkingTicket ticket = utilAutowire.getTicketUtil().generateTicket(spot,vehicle,repositoryAutowire.getParkingTicketRepository());
+        if (spot != null) {
+            ParkingTicket ticket = utilAutowire.getTicketUtil().generateTicket(spot, vehicle, repositoryAutowire.getParkingTicketRepository());
             spot.setSpotState(SpotState.occupied);
             res = "Parked vehicle. Ticket ID: " + ticket.getParkingTicketId();
         }
@@ -100,19 +85,22 @@ public class ParkingServiceIMPL implements ParkingService{
 
     @Override
     public void displayTicket(String ticketId) {
-        ParkingTicket ticket = utilAutowire.getFindingUtil().getTicket(ticketId,repositoryAutowire.getParkingTicketRepository());
+        ParkingTicket ticket = utilAutowire.getFindingUtil().getTicket(ticketId, repositoryAutowire.getParkingTicketRepository());
         utilAutowire.getDisplayUtil().displayTicketDetails(ticket);
     }
 
     @Override
     public String unparkVehicle(String ticketCode) {
 
-        ParkingTicket ticket = utilAutowire.getFindingUtil().getTicket(ticketCode,repositoryAutowire.getParkingTicketRepository());
+        ParkingTicket ticket = utilAutowire.getFindingUtil().getTicket(ticketCode, repositoryAutowire.getParkingTicketRepository());
+
+        ParkingSpot spot = ticket.getParkingSpot();
+
         String unparkResponse = "!----- Invalid Ticket -----!";
-        if(ticket!=null){
-            ParkingSpot spot = ticket.getParkingSpot();
+
+        if (ticket != null && spot.getSpotState() == SpotState.occupied) {
             spot.setSpotState(SpotState.free);
-            unparkResponse = "Unparked vehicle with Registration Number: "+ticket.getVehicleEntity().getVehicleRegNumber()+" and Color: "+ticket.getVehicleEntity().getVehicleColor();
+            unparkResponse = "Unparked vehicle with Registration Number: " + ticket.getVehicleEntity().getVehicleRegNumber() + " and Color: " + ticket.getVehicleEntity().getVehicleColor();
         }
         return unparkResponse;
     }
