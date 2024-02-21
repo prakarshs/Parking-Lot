68045304bd50b893acb74dc40e5c24647322179b
jhethia <jhethia@gmail.com>
2024-02-21 13:55:58 +0530

    Corrected repository codition

--- src/main/java/org/LLD/Util/FindingUtil.java
+++ src/main/java/org/LLD/Util/FindingUtil.java
@@ -9,30 +9,29 @@ import java.util.*;
 
 public class FindingUtil {
     public Map<Integer, List<Integer>> getFreeCountResult(VehicleType vehicleType, ParkingSpotRepository parkingLotRepository) {
-        System.out.println("hi");
-        var vehicleSpotsTotal = parkingLotRepository.getParkingSpotMap().get(vehicleType);
+        var vehicleTotalSlots = parkingLotRepository.getParkingSpotMap().get(vehicleType);
+        if(!vehicleTotalSlots.isEmpty()) {
+            Map<Integer, List<Integer>> result = new HashMap<>();
 
-        System.out.println("hello");
-        Map<Integer, List<Integer>> result = new HashMap<>();
-
-        for (Map.Entry<Integer, ParkingSpot> spotEntry : vehicleSpotsTotal.entrySet()) {
-
-            Integer floorId = spotEntry.getValue().getParkingFloorId();
-            if (spotEntry.getValue().getSpotState() == SpotState.free) {
-                if (!result.containsKey(floorId)) {
-                    result.put(floorId, new ArrayList<>());
+            for (Map.Entry<Integer, ParkingSpot> spotEntry : vehicleTotalSlots.entrySet()) {
+                Integer floorId = spotEntry.getValue().getParkingFloorId();
+                if (spotEntry.getValue().getSpotState() == SpotState.free) {
+                    if (!result.containsKey(floorId)) {
+                        result.put(floorId, new ArrayList<>());
+                    }
+                    result.get(floorId).add(spotEntry.getValue().getSpotId());
                 }
-                result.get(floorId).add(spotEntry.getValue().getSpotId());
             }
+            return result;
         }
-        return result;
+        else return null;
 
     }
 
     public Map<Integer, List<Integer>> getFreeSlotsResult(VehicleType vehicleType, ParkingSpotRepository parkingSpotRepository) {
 
         var vehicleTotalSlots = parkingSpotRepository.getParkingSpotMap().get(vehicleType);
-        if(vehicleTotalSlots!=null) {
+        if(!vehicleTotalSlots.isEmpty()) {
             Map<Integer, List<Integer>> result = new HashMap<>();
 
             for (Map.Entry<Integer, ParkingSpot> spotEntry : vehicleTotalSlots.entrySet()) {
@@ -53,7 +52,7 @@ public class FindingUtil {
     public Map<Integer, List<Integer>> getOccupiedSlotsResult(VehicleType vehicleType, ParkingSpotRepository parkingSpotRepository) {
 
         var vehicleTotalSlots = parkingSpotRepository.getParkingSpotMap().get(vehicleType);
-        if(vehicleTotalSlots!=null) {
+        if(!vehicleTotalSlots.isEmpty()) {
             Map<Integer, List<Integer>> result = new HashMap<>();
             for (Map.Entry<Integer, ParkingSpot> spotEntry : vehicleTotalSlots.entrySet()) {
 
