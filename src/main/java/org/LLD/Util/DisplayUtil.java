c7ded83fe6cb244c6d1faaa2004010c079602ee3
jhethia <jhethia@gmail.com>
2024-02-21 13:58:11 +0530

    Error handled

--- src/main/java/org/LLD/Util/DisplayUtil.java
+++ src/main/java/org/LLD/Util/DisplayUtil.java
@@ -8,7 +8,7 @@ import java.util.Map;
 public class DisplayUtil {
     FindingUtil findingUtil = new FindingUtil();
     public void displayFreeCount(Map<Integer,List<Integer>> result, VehicleType vehicleType) {
-        if(result!=null) {
+        if(!result.isEmpty()) {
             for (Map.Entry<Integer, List<Integer>> resultEntry : result.entrySet()) {
                 System.out.println("No. of free slots for " + vehicleType + " on Floor " + resultEntry.getKey() + ": " + resultEntry.getValue().size());
             }
@@ -17,7 +17,7 @@ public class DisplayUtil {
     }
 
     public void displayFreeSlots(Map<Integer,List<Integer>> result, VehicleType vehicleType) {
-        if(result!=null) {
+        if(!result.isEmpty()) {
             for (Map.Entry<Integer, List<Integer>> resultEntry : result.entrySet()) {
                 System.out.print("Free slots for " + vehicleType + " on Floor " + resultEntry.getKey() + ": ");
                 resultEntry.getValue().forEach(spotId -> {
@@ -32,7 +32,7 @@ public class DisplayUtil {
     }
 
     public void displayOccupiedSlots(Map<Integer, List<Integer>> result, VehicleType vehicleType) {
-        if(result!=null) {
+        if(!result.isEmpty()) {
             for (Map.Entry<Integer, List<Integer>> resultEntry : result.entrySet()) {
                 System.out.print("Occupied slots for " + vehicleType + " on Floor " + resultEntry.getKey() + ": ");
                 resultEntry.getValue().forEach(spotId -> {
